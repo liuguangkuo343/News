66d2694a16809ed72e99df63d0f6b6a8eebd0608
fanyoufu <fanyoufu@outlook.com>
2019-11-21 17:09:39 +0800

    文章类型-新增

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -8,5 +8,7 @@ var APIURLS = {
    user_login: baseUrl + 'admin/login',
    user_getInfo: baseUrl + 'admin/getuser',
 
-   category_show:  baseUrl + 'admin/category_search'
+   category_show:  baseUrl + 'admin/category_search',
+   category_del : baseUrl + 'admin/category_delete',
+   category_add : baseUrl + 'admin/category_add'
 }
